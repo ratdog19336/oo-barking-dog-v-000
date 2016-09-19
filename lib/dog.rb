bae5bf3b6d22976f3d5351d784ffe7ac9a861419
Nicholas Ratti <nickratti@gmail.com>
2016-09-19 15:37:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Dog
+  attr_accessor :name, :bark
+
+  def bark
+    puts "woof!"
+  end
+
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.bark = "woof!"
+
+
+#def initialize
+#  @bark
+#  def bark=(bark)
+#    puts "woof!"
+#  end
+#  def bark
+#    @bark
+#  end
+#end
