f5d04a64cde06379d4e37a50d15f6497fbb54297
Nicholas Ratti <nickratti@gmail.com>
2019-09-01 22:02:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,8 @@
 class Dog
-  attr_accessor :name, :bark
+  attr_accessor :name
 
   def bark
     puts "woof!"
   end
 
 end
-
-fido = Dog.new
-fido.name = "Fido"
-fido.bark = "woof!"
-
-
-#def initialize
-#  @bark
-#  def bark=(bark)
-#    puts "woof!"
-#  end
-#  def bark
-#    @bark
-#  end
-#end
